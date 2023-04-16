0e7f24b1f2d6bbd4339abe58eace528227ab6b43
Iván Ortiz del Noval <ivan.ortizd@alumnos.unican.es>
2023-04-16 20:12:47 +0200

    Añadir tutorial resuelto y esqueleto del módulo de Gobernanza
    Los módulos ya pueden instalarse en Odoo. Se puede comenzar su desarrollo sin problemas.

---
+++ custom_addons/data_gov/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# More info at https://www.odoo.com/documentation/master/reference/module.html
+
+{
+    "name": "Data Governance",
+    "category": '',
+    "depends": [
+        "base"
+    ],
+    "data": [
+        # rellenar con cosas de archivos y eso
+
+    ],
+    "application": True,
+}
