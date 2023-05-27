604a0d81ab22ec81bb37a033704a4912e556ba26
Iván Ortiz del Noval <ivan.ortizd@alumnos.unican.es>
2023-05-28 01:34:09 +0200

    Ampliar con DataEntity PERO NO CON TODAS LAS RELACIONES
    La agregación o las 1-N no están hechas, ya que la inserción desde la interfaz no la veo posible.
    Cambiar a N-N o invertir la dirección de la visibilidad.

--- custom_addons/data_gov/dominio/data_entity.py
+++ custom_addons/data_gov/dominio/data_entity.py
@@ -15,7 +15,7 @@ class DataEntity(models.Model):
     name = fields.Text('Nombre', required=True)
     category = fields.Many2one('datagov.category', 'Categoría', required=True)  # clase Category
     description = fields.Text('Description', required=True)
-    securityClassification = fields.Many2one('datagov.security.classification', 'Clasificación se seguridad',
+    securityClassification = fields.Many2one('datagov.security.classification', 'Clasificación de seguridad',
                                              required=True)
     steward = fields.Many2one('datagov.actor', 'Administrador', required=True)
     isDigital = fields.Boolean('Es digital', required=True)
@@ -23,13 +23,18 @@ class DataEntity(models.Model):
     # Atributos de relaciones con otras clases del modelo
     masterDataSource = fields.Many2one('datagov.data.source', 'Fuente de datos maestra', required=True)
     term = fields.Many2one('datagov.glossary.term', 'Término del glosario', required=True)
+    dataQualityRule = fields.One2many('datagov.data.quality.rule', 'name', string='Reglas de calidad')
 
     # Many2many para information asset
     '''performs = fields.Many2many(comodel_name='datagov.role', relation='datagov_role_actor', column1='id_actor',
                                 column2='id_role', string='Roles')'''
+
+    '''
     # One2many sobre la misma tabla para la agregación TODO ver como evitar recursión
-    composedOf = fields.One2many('datagov.data.entity', 'name', string='Compuesta por')
-    dataQualityRule = fields.One2many('datagov.data.quality.rule', 'name', string='Reglas de calidad')
+    # Hay que crear un atributo adicional para la padre, pero esconderlo en las vistas
+    parentEntity = fields.Many2one('datagov.data.entity', string='Entidad padre')  # la padre
+    composedOf = fields.One2many('datagov.data.entity', 'name', string='Compuesta por')  # la compuesta
+    '''
 
     # restricción
     @api.onchange('category')
@@ -40,3 +45,15 @@ class DataEntity(models.Model):
             # lanzar notificación
             raise UserError("La categoría del actor debe ser una del tipo 'Entidad de datos'.")
         return
+
+    '''
+    # que no pueda estar compuesta por si mismo
+    @api.onchange('composedOf')
+    def on_change_composed_of(self):
+        for i in self.composedOf:
+            # ver que no se ha metido si mismo
+            if i.name == self.name:
+                # lanzar notificación
+                raise UserError("Una entidad de datos no puede estar compuesta por sigo misma.")
+        return
+    '''
