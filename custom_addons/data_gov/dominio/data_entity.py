5b8ce874857f53b1c3860c209511a64b055a79eb
Iván Ortiz del Noval <ivan.ortizd@alumnos.unican.es>
2023-06-04 14:29:59 +0200

    Arreglar definitivamente la agregación consigo misma en Data Entity.

--- custom_addons/data_gov/dominio/data_entity.py
+++ custom_addons/data_gov/dominio/data_entity.py
@@ -29,11 +29,12 @@ class DataEntity(models.Model):
     '''performs = fields.Many2many(comodel_name='datagov.role', relation='datagov_role_actor', column1='id_actor',
                                 column2='id_role', string='Roles')'''
 
-    # One2many sobre la misma tabla para la agregación TODO ver como evitar recursión
-    # Hay que crear un atributo adicional oculto para la padre, pero esconderlo en las vistas
-    _compone = fields.Many2one('datagov.data.entity', string='Entidad que compone')
-    composedOf = fields.One2many('datagov.data.entity', 'name', string='Compuesta por',
-                                 context={'default__compone': lambda self: self.id})  # context para poder añadir desde la vista
+    # Tiene que ser Many2Many a los dos lados para poder añadir tal y como queremos
+    # Hay que crear un atributo adicional oculto para la padre, pero esconderlo en las vistas, limite de un elemento
+    compone = fields.Many2many(relation='datagov_data_entity_aggregation', comodel_name='datagov.data.entity',
+                               column1='id_compone', column2='id_padre', string='Entidad que compone')
+    composedOf = fields.Many2many(relation='datagov_data_entity_aggregation', comodel_name='datagov.data.entity',
+                                  column1='id_padre', column2='id_compone', string='Compuesta por')
 
     # restricción
     @api.onchange('category')
@@ -45,14 +46,22 @@ class DataEntity(models.Model):
             raise UserError("La categoría del actor debe ser una del tipo 'Entidad de datos'.")
         return
 
-    '''
-    # que no pueda estar compuesta por si mismo
+    # Que la relación consigo para la agregación siga siendo 1-N
     @api.onchange('composedOf')
-    def on_change_composed_of(self):
-        for i in self.composedOf:
-            # ver que no se ha metido si mismo
-            if i.name == self.name:
-                # lanzar notificación
-                raise UserError("Una entidad de datos no puede estar compuesta por sigo misma.")
-        return
-    '''
+    def _check_many2many_limit(self):
+        lista_compone = self.compone.ids  # hay que obtener la lista con los id, el método len() no va con many2many
+        lista_compuesta = self.composedOf.ids
+        # hay que pasar por cada una de las que lo componen para comprobar esto
+        for comp_id in lista_compuesta:
+            obj = self.env['datagov.data.entity'].browse(comp_id)  # obtener objeto completo, no la parte de inner join
+            # print('ID:', obj.name, 'Compone: ', str(obj.compone.ids), ' len: ', len(obj.compone))
+            if len(obj.compone) > 0:  # 0 porque en este punto no contiene la que se va a añadir
+                texto_error = 'Una entidad de datos no puede ser parte de la composición de dos o más entidades (' + \
+                    obj.name + ').'
+                raise UserError(texto_error)
+        # que si eres una entidad compuesta no puedas componer otras y viceversa
+        if len(lista_compuesta) > 0 and len(lista_compone) > 0:
+            raise UserError('Una entidad de datos compuesta no puede ser parte de otras entidades.')
+        # que no sea consigo misma
+        if self.compone.__contains__(self):
+            raise UserError('Una entidad de datos no puede ser compuesta por sí misma.')
